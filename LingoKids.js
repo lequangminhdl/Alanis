66a6eadbd43e805a84a18598c49f484e140d3f63
lequangminhdl <61529068+lequangminhdl@users.noreply.github.com>
2020-10-01 13:04:04 +0700

    Lingokids_test

---
+++ LingoKids.js
@@ -0,0 +1,18 @@
+var obj = JSON.parse($response.body);
+
+obj["subscriptions"]= [
+      {
+        "id": 805063,
+        "status": "trial",
+        "state": "active_trial"
+        "product": "unlimited",
+        "platform": "apple",
+        "starts_at": 1572617692,
+        "ends_at": 4099821292,
+        "plan_id": "lk.ios.s1m.t1m.p15.v1",
+        "duration_unit": "months",
+        "duration_value": 1,
+        "auto_renew_status": true,        
+      }];
+
+$done({body: JSON.stringify(obj)});
