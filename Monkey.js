a4136ef02cf20cabe710abf549b4e341cee4084e
lequangminhdl <61529068+lequangminhdl@users.noreply.github.com>
2020-10-01 14:53:00 +0700

    Update Monkey.js

--- Monkey.js
+++ Monkey.js
@@ -3,9 +3,9 @@
 var obj = JSON.parse($response.body);
 
 obj["purchased"]= [
-	{
-	  "pay_inapp": true,
-	  "time_expire": 0,
+    {
+      "pay_inapp": true,
+      "time_expire": 0,
       "product_list": [],
       "is_active": true,
       "is_free": false
