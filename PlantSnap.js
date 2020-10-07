7ff3537b90a9710fc7a41b32f46c072913122198
lequangminhdl <61529068+lequangminhdl@users.noreply.github.com>
2020-10-07 09:22:37 +0700

    Update PlantSnap.js

--- PlantSnap.js
+++ PlantSnap.js
@@ -1,9 +1,11 @@
 var obj = JSON.parse($response.body);
 
-obj.subscription=
+obj.subscription.userType=
       {
         "active":true,
         "plan":"monthly",
-        "validUntil":4099821292
+        "validUntil":4099821292,
+        "code":1,
+        "name":"premium"
       };
 $done({body: JSON.stringify(obj)});
