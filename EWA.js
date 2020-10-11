4187d523ca83f8594e797c46f877eb844f58f1a6
lequangminhdl <61529068+lequangminhdl@users.noreply.github.com>
2020-10-11 10:26:23 +0700

    Update EWA.js

--- EWA.js
+++ EWA.js
@@ -1,6 +1,13 @@
 var obj = JSON.parse($response.body);
+
+obj.result.user.subscription = "premium";
+
 obj.result.user.bill = {
-		"subscription": "premium",
-		"endDate": "2029-05-07T10:28:22+0000"
+	"subscription": "premium"
+	"createDate": "2019-04-29T10:28:24+0000",
+	"startDate": "2019-04-29T10:28:22+0000",
+	"endDate": "2029-05-07T10:28:22+0000",
+	"daysLeft": 365,
+	"period": "6"
 	};
 $done({body: JSON.stringify(obj)});
