6e45dc678080cc07763f630af79a9f1a6f87400f
lequangminhdl <61529068+lequangminhdl@users.noreply.github.com>
2020-10-16 21:04:13 +0700

    Update Babybus.js

--- Babybus.js
+++ Babybus.js
@@ -3,8 +3,8 @@ var obj = JSON.parse($response.body);
 obj.data = [
 {
 	"is_vip": true,
-	"is_subscribe": 1
+	"is_subscribe": 1,
 	"end_time": 3790570877,
-	"start_time": 1581442291,
+	"start_time": 1581442291
 }];
 $done({body: JSON.stringify(obj)});
