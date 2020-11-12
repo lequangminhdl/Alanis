4ca3c5425afd44a33f695f3e39deabb55364d727
lequangminhdl <61529068+lequangminhdl@users.noreply.github.com>
2020-11-12 19:58:15 +0700

    Update Babybus_CheckOrder.js

--- Babybus_CheckOrder.js
+++ Babybus_CheckOrder.js
@@ -1,9 +1,13 @@
 var obj = JSON.parse($response.body);
-obj["data"] = [
-  {
+obj = {
+  "status" : "1",
+  "info" : "调试信息",
+  "data" : [
+    {
       "transaction_id" : "",
       "notify_status" : 0,
       "order_status" : 1
-     }
-],
+    }
+  ]
+};
 $done({body: JSON.stringify(obj)});
