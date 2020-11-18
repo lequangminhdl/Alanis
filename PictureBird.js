3bc753f516c775be4327dd091cc6e67f35ce600f
lequangminhdl <61529068+lequangminhdl@users.noreply.github.com>
2020-11-18 19:11:13 +0700

    Update PictureBird.js

--- PictureBird.js
+++ PictureBird.js
@@ -1,16 +1,8 @@
 var obj = JSON.parse($response.body);
-obj.response = {
-   "is_check" : "1",
-	"limit_times_type" : {
-      "login_limit_times" : 0,
-      "ad_limit_times" : 0,
-      "register_limit_times" : 0,
-      "share_limit_times" : 0,
-      "identify_limit_times" : 0
-    },
-    "left_times_type_daily" : {
-      "share_limit_times" : 0,
-      "ad_limit_times" : 0
-    },
+obj.response.user = {
+      "is_has_tried" : true,
+      "role" : "premium",
+      "nickname" : "Le Minh",
+      "status" : 1
 };
 $done({body: JSON.stringify(obj)});
