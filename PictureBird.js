ccaabc5e962985b46e2110b56af2841afe9ca6c8
lequangminhdl <61529068+lequangminhdl@users.noreply.github.com>
2020-11-18 19:26:03 +0700

    Update PictureBird.js

--- PictureBird.js
+++ PictureBird.js
@@ -2,7 +2,6 @@ var obj = JSON.parse($response.body);
 obj.response.user = {
       "is_has_tried" : true,
       "role" : "premium",
-      "nickname" : "Le Minh",
       "status" : 1
 };
 $done({body: JSON.stringify(obj)});
