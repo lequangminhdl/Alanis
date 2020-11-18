ddaa1f2d1d0f61ec665b7c56c1d99a7924aff09a
lequangminhdl <61529068+lequangminhdl@users.noreply.github.com>
2020-11-18 19:36:07 +0700

    Update PictureBird.js

--- PictureBird.js
+++ PictureBird.js
@@ -6,7 +6,7 @@ obj.response.user = {
       "user_id" : 581336,
       "is_random_nickname" : false,
       "cid" : "bc6c8ea1-6436-92c6-1d9a-61c224566623",
-      "is_has_tried" : true,
+      "is_has_tried" : false,
       "signature" : "",
       "background_url" : "",
       "limit_identify_count" : 0,
