5f16c833de0b68802884e3e1dc269f4320854417
lequangminhdl <61529068+lequangminhdl@users.noreply.github.com>
2020-11-19 21:43:50 +0700

    Update DownDog.js

--- DownDog.js
+++ DownDog.js
@@ -1,3 +1,12 @@
 var obj = JSON.parse($response.body);
-obj.displayMembership = true;
+obj.subscriptions = {
+      "active" : true,
+      "willBeChargedAgain" : false,
+      "endDate" : "14 Dec 2090",
+      "startDate" : "18 Nov 2020",
+      "canSetAutoRenew" : false,
+      "planButtonSubtext" : "Expiring 14 Dec 2090",
+      "planButtonText" : "Free Trial (Yoga)",
+      "trial" : false
+};
 $done({body: JSON.stringify(obj)});
