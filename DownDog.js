423b06b4183e884d90196a2fda8e30e1bcf8b958
lequangminhdl <61529068+lequangminhdl@users.noreply.github.com>
2020-11-19 22:12:46 +0700

    Update DownDog.js

--- DownDog.js
+++ DownDog.js
@@ -1,10 +1,6 @@
 var obj = JSON.parse($response.body);
-obj = {
-"subscriptions" : [
-    {
+obj.subscriptions = {
       "endDate" : "14 Dec 2090",
       "planButtonSubtext" : "Expiring 14 Dec 2090"
-      }
-  ],
-};
+ };
 $done({body: JSON.stringify(obj)});
