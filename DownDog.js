654eff24f8a4d6fe93f0e20213e99517b60162e2
lequangminhdl <61529068+lequangminhdl@users.noreply.github.com>
2020-11-19 21:46:25 +0700

    Add files via upload

---
+++ DownDog.js
@@ -0,0 +1,12 @@
+var obj = JSON.parse($response.body);
+obj.subscriptions = {
+      "active" : true,
+      "willBeChargedAgain" : false,
+      "endDate" : "14 Dec 2090",
+      "startDate" : "18 Nov 2020",
+      "canSetAutoRenew" : false,
+      "planButtonSubtext" : "Expiring 14 Dec 2090",
+      "planButtonText" : "Free Trial (Yoga)",
+      "trial" : true
+};
+$done({body: JSON.stringify(obj)});
