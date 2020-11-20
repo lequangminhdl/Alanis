9a643f2a40fbd7f5fda1544f5397cab25a40eee6
lequangminhdl <61529068+lequangminhdl@users.noreply.github.com>
2020-11-20 20:58:23 +0700

    Update MISA.js

--- MISA.js
+++ MISA.js
@@ -1,7 +1,4 @@
 var obj = JSON.parse($response.body);
-obj = {
-  "TotalCoin" : 1000000,
-  "IsPremium" : true,
-  "IsoEndDate" : "01/01/1753 12:00:00 AM"
-};
+obj.IsPremium = true,
+obj.IsoEndDate = "01/01/2084 12:00:00 AM"
 $done({body: JSON.stringify(obj)});
