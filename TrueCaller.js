187adb2b0cd06e98b7a863f08c015fdb0e829b13
lequangminhdl <61529068+lequangminhdl@users.noreply.github.com>
2020-11-20 22:17:42 +0700

    Update TrueCaller.js

--- TrueCaller.js
+++ TrueCaller.js
@@ -1,10 +1,16 @@
 var obj = JSON.parse($response.body);
 obj.proStatus = {
-	"renewable" : false,
+    "renewable" : false,
+    "start" : "1970-01-01T00:00:00Z",
     "isGracePeriodExpired" : false,
     "level" : "gold",
     "isExpired" : false,
     "gracePeriodExpires" : "2070-01-01T00:00:00Z",
+    "contactRequestQuota" : {
+      "month" : 30,
+      "Year" : 365
+    },
+    "credits" : 0,
     "expires" : "2070-01-01T00:00:00Z",
     "isFreeTrial" : false
   };
