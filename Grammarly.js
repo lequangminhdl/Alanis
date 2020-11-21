ed385e24b0dd1273a00e477c5f67bf4d4f6b61ee
lequangminhdl <61529068+lequangminhdl@users.noreply.github.com>
2020-11-21 23:07:02 +0700

    Add files via upload

---
+++ Grammarly.js
@@ -0,0 +1,27 @@
+var obj = JSON.parse($response.body);
+obj = {
+"isPremium" : true,
+  "currentPlan" : {
+    "regularPlanId" : 5,
+    "regularPrice" : 139.94999999999999,
+    "regularPriceMoney" : {
+      "currency" : "USD",
+      "value" : 139.94999999999999
+    },
+    "id" : 5,
+    "price" : 139.94999999999999,
+    "title" : "Annual",
+    "firstThreeMonthsPromo" : 0,
+    "priceMoney" : {
+      "currency" : "USD",
+      "value" : 139.94999999999999
+    },
+    "description" : "1-year Grammarly Subscription",
+    "periodMonths" : 12,
+    "hasTrial" : false
+  },
+  "nextPayDate" : "Oct 9, 2084 6:40:59 AM",
+  "isAppleSubscription" : false,
+  "isCancelled" : false
+};
+$done({body: JSON.stringify(obj)});
