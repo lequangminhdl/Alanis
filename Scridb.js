68c57407607367ca51e9acefc35fab9335886a8f
lequangminhdl <61529068+lequangminhdl@users.noreply.github.com>
2020-11-22 09:18:48 +0700

    Update Scridb.js

--- Scridb.js
+++ Scridb.js
@@ -1,22 +1,20 @@
 var obj = JSON.parse($response.body);
 obj.result = {
-    "state" : "active",
-    "limited_validity" : null,
-    "next_payment_due" : 41632530288,
-    "last_paid_date" : 1600994294,
-    "total_price" : {
-      "value" : 84.989999999999995,
-      "currency" : "USD"
-    },
-    "can_be_canceled" : false,
-    "description" : "Annual recurring membership",
-    "type" : "credit card",
-    "title" : "Annual Pass",
-    "purchase_valid_to" : null,
-    "can_be_renewed" : false,
-    "subscription" : true,
-    "subscription_free_trial_days" : null,
-    "product_handle" : "scribd-pmp-annual-subscription",
-    "subscription_duration" : "1.year"
+   "membership_info" : {
+      "status" : "member",
+      "resume_date" : null,
+      "credit_cache_bust" : "c3b1895ab5c2fb817ea750a9768aa1b8",
+      "bill_date" : 1606787286,
+      "pays_additional_tax" : true,
+      "has_pmp_access" : true,
+      "end_date" : null,
+      "bill_method" : "credit card",
+      "next_bill_price" : {
+        "value" : 8.9900000000000002,
+        "currency" : "USD"
+      }
+    "is_referral_creditable" : true,
+    "credit_next_accrual_date" : 1606787286,
+    "is_paused" : false,
 };
 $done({body: JSON.stringify(obj)});
