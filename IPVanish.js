26a50181d468fb089e2bced317506bfb5d063c3e
lequangminhdl <61529068+lequangminhdl@users.noreply.github.com>
2020-11-22 15:07:36 +0700

    Delete IPVanish.js

--- IPVanish.js
+++
@@ -1,7 +0,0 @@
-var obj = JSON.parse($response.body);
-obj = {
-  "sub_end_epoch" : 4631208604,
-  "access_expire_epoch" : 4606559509,
-  "account_type" : 2,
- };
-$done({body: JSON.stringify(obj)});
