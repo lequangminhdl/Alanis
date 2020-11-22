e043c36e6ee5f9a5abf16d9b0afb6a641ee43c4d
lequangminhdl <61529068+lequangminhdl@users.noreply.github.com>
2020-11-22 09:02:44 +0700

    Update Drafts.js

--- Drafts.js
+++ Drafts.js
@@ -2,7 +2,7 @@ var obj = JSON.parse($response.body);
 obj = {
  "active_expires_at" : "2084-01-01T00:00:00Z",
   "is_subscription_active" : true,
-  "active_subscription_type" : "pro",
+  "active_subscription_type" : "",
   "is_blocked" : false,
   "has_had_free_trial" : false
 };
