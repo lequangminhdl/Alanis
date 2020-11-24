f4fe34d09785cbdc65f19fb53c6687b1d576f2a3
lequangminhdl <61529068+lequangminhdl@users.noreply.github.com>
2020-11-24 19:25:47 +0700

    Update StepUp.js

--- StepUp.js
+++ StepUp.js
@@ -6,8 +6,18 @@ const path1 = 'book/api/v1/roadmap/';
 let obj = JSON.parse(body);
 
 if (url.indexOf(path1) != -1) {
-    obj.data.detail.children.books.blocks["is_lock"] = false;
+    obj = {
+    "data" : {
+    	"detail" : [
+    	{
+    		"children" : [
+    		{
+    			"books" : [
+    			{
+    				"blocks" : [
+    				{
+    					"is_lock" = false
+                    }]}]}]}]}};
     body = JSON.stringify(obj);  
  }
-
 $done({body});
