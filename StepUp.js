e835adff50e5d28f9c235c426b1b3966bf30324a
lequangminhdl <61529068+lequangminhdl@users.noreply.github.com>
2020-11-24 20:08:58 +0700

    Update StepUp.js

--- StepUp.js
+++ StepUp.js
@@ -1,5 +1,5 @@
-body = $response.body.replace(/while.{7}\n/, "");
-let obj = JSON.parse(body);
-    obj.data.detail.children.books.blocks[["is_lock"]] = false
+let obj=JSON.parse($response.body);
+let usr=obj.data.body;
+   usr.detail.children.books.blocks[["is_lock"]] = false
 body = JSON.stringify(obj); 
-$done({body});
+$done({body: JSON.stringify(obj)});
