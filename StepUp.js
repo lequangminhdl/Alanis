cd51c3dd6058cd1a7d5e8a8de86e9f7580dd1c60
lequangminhdl <61529068+lequangminhdl@users.noreply.github.com>
2020-11-24 20:09:35 +0700

    Update StepUp.js

--- StepUp.js
+++ StepUp.js
@@ -1,5 +1,5 @@
 let obj=JSON.parse($response.body);
 let usr=obj.data.body;
-   usr.detail.children.books.blocks[["is_lock"]] = false
+   usr.detail.children.books.blocks["is_lock"] = false
 body = JSON.stringify(obj); 
 $done({body: JSON.stringify(obj)});
