27a1c07b0590fa9bd8856df2cf37cc79f0b5cbe0
lequangminhdl <61529068+lequangminhdl@users.noreply.github.com>
2022-10-14 16:10:44 +0700

    Update YL.js

--- YL.js
+++ YL.js
@@ -1,6 +1,6 @@
 var obj = JSON.parse($response.body);
 
-obj.result.premiumEndDate = "2022-10-15T04:28:21.999582Z";
+obj.result.premiumEndDate = "2022-11-15T04:28:21.999582Z";
 obj.result.isPremiumActive = true;
 
 $done({body: JSON.stringify(obj)});
